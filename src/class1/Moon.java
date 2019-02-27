7aeda40e047d96a1ea7b43a9bb725e979f82b554
vemeister <edelman.vladimir@gmail.com>
2019-02-27 21:21:53 +0100

    add two missing fields

--- src/class1/Moon.java
+++ src/class1/Moon.java
@@ -3,8 +3,13 @@ package class1;
 public final class Moon {
     public String name = "Moon";
     public char letter = 'M';
+
     public byte orbitalSpeed = 1;
     public int  Perigee = 362_000;
+    public long mass = 7_342_000_000_000_000_000L;
+    
+    public double orbitalPeriod = 27.321_661;
+    public float escapeVelocity = 2.38f;
+
     public boolean isLivable = false;
-    public double orbitalPeriod = 27.321661;
 }
