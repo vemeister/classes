229712028a71d4b1454a0231bb8c8552d5e5c6cc
vemeister <48064432+vemeister@users.noreply.github.com>
2019-02-27 21:53:24 +0100

    Update Moon.java

--- src/class1/Moon.java
+++ src/class1/Moon.java
@@ -5,7 +5,7 @@ public final class Moon {
     public char letter = 'M';
 
     public byte orbitalSpeed = 1;
-    public int  Perigee = 362_000;
+    public int  perigee = 362_000;
     public long mass = 7_342_000_000_000_000_000L;
     
     public double orbitalPeriod = 27.321_661;
